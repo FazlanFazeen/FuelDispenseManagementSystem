530cd17fc7b3a9a79db245b869684d73f4e9c39c
FazlanFazeen <mohomad20210137@iit.ac.lk>
2022-12-17 14:47:25 +0530

    Fuel Dispense Manager, Octane and Diesel Dispense Manager
    Adding the methods to the FuelDispenseManager Interface. Adding the methods, variables, getters and setters for the octane and dieselDispense Managers.

--- src/DieselFuelDispenseManager.java
+++ src/DieselFuelDispenseManager.java
@@ -1,2 +1,69 @@
 public class DieselFuelDispenseManager implements FuelDispenseManager{
+
+//Instance variable
+
+    private int dispenserNum;
+    private String fuelType;
+    private double priceOfLitre;
+    private String operating;
+    private double dispensedAmount;
+    private FuelRepository Repository;
+
+    //Constructor
+
+    public DieselFuelDispenseManager(int dispenserNum, String fuelType, double priceOfLitre, String operating, double dispensedAmount, FuelRepository Repository) {
+        this.dispenserNum = dispenserNum;
+        this.fuelType = fuelType;
+        this.priceOfLitre = priceOfLitre;
+        this.operating = operating;
+        this.dispensedAmount = dispensedAmount;
+        this.Repository = Repository;
+    }
+
+    //Getter and Setter
+
+    public int getDispenserNum() {
+        return dispenserNum;
+    }
+
+    public void setDispenserNum(int dispenserNum) {
+        this.dispenserNum = dispenserNum;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public double getPriceOfLitre() {
+        return priceOfLitre;
+    }
+
+    public void setPriceOfLitre(double priceOfLitre) {
+        this.priceOfLitre = priceOfLitre;
+    }
+
+    public String getOperating() {
+        return operating;
+    }
+
+    public void setOperating(String operating) {
+        this.operating = operating;
+    }
+
+    public double getDispensedAmount() {
+        return dispensedAmount;
+    }
+
+    public void setDispensedAmount(double dispensedAmount) {
+        this.dispensedAmount = dispensedAmount;
+    }
+
+    @Override
+    public void installingDispenser() {
+
+    }
 }
--- src/FuelDispenseManager.java
+++ src/FuelDispenseManager.java
@@ -1,2 +1,3 @@
 public interface FuelDispenseManager {
+    public void installingDispenser();
 }
