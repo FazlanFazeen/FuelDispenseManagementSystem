6f3d85767c5ee7d31943e19098a8f1c9f975ab11
YaseenMunowwar <107970104+yaseen559@users.noreply.github.com>
2022-12-18 11:27:09 +0530

    queue and common waiting queue
    queue and common waiting queue classes

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
 public class Queue {
+    private int queueNo;
+    private int NoOfVehicles;
+    private boolean isFull;
+    private ArrayList<Customer> listOfcustomers = new ArrayList<>(10);
+    private CommonWaitingQueue commonqueue;
+    //constructors
+    public Queue(int queueNo, int noOfVehicles, boolean isFull,
+                 ArrayList<Customer> listOfcustomers, CommonWaitingQueue commonqueue) {
+        this.queueNo = queueNo;
+        NoOfVehicles = noOfVehicles;
+        this.isFull = isFull;
+        this.listOfcustomers = listOfcustomers;
+        this.commonqueue = commonqueue;
+    }
+//getters and setters
+
+    public CommonWaitingQueue getCommonqueue() {
+        return commonqueue;
+    }
+    public void setCommonqueue(CommonWaitingQueue commonqueue) {
+        this.commonqueue = commonqueue;
+    }
+    public int getQueueNo() {
+        return queueNo;
+    }
+    public void setQueueNo(int queueNo) {
+        this.queueNo = queueNo;
+    }
+    public int getNoOfVehicles() {
+        return NoOfVehicles;
+    }
+    public void setNoOfVehicles(int noOfVehicles) {
+        NoOfVehicles = noOfVehicles;
+    }
+    public boolean isFull() {
+        return isFull;
+    }
+    public void setFull(boolean full) {
+        isFull = full;
+    }
+    public ArrayList<Customer> getListOfcustomers() {
+        return listOfcustomers;
+    }
+    public void setListOfcustomers(ArrayList<Customer> listOfcustomers) {
+        this.listOfcustomers = listOfcustomers;
+    }
+    public void receiveTicket(){
+        //recieve the ticket
+    }
 }
